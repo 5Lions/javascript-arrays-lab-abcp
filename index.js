d0316902a812c49920fac1d77cd6248fcc7a3cb2
A Lee <ayslee@gmail.com>
2018-01-16 13:48:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,43 @@ const app = "I don't do much.";
 var kittens = ['Milo', 'Otis', 'Garfield'];
 console.log(kittens);
 
-destructivelyAppendKitten(array, element); {
+function destructivelyAppendKitten(array, element) {
   array.push(element);
   return array;
 }
-
 console.log(destructivelyAppendKitten(kittens, "Ralph"));
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyPrependKitten(array, element) {
+  array.unshift(element);
+  return array;
+}
+console.log(destructivelyPrependKitten(kittens, "Bob"));
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyRemoveLastKitten(array) {
+  array.pop();
+  return array;
+}
+console.log(destructivelyRemoveLastKitten(kittens));
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyRemoveFirstKitten(array) {
+  array.shift();
+  return array;
+}
+console.log(destructivelyRemoveFirstKitten(kittens));
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function appendKitten(array, element) {
+  return [...array, element];
+}
+console.log(appendKitten(kittens, "Broom"));
+console.log(kittens);
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function prependKitten(array, element) {
+  return [element, ...array]
+}
+
+console.log(prependKitten(kittens, "Arnold"));
